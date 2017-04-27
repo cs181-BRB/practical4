8e88ebc6f26617b766a5502cf5e11c98b03ea3ed
Roger Z <the.roger.zou@gmail.com>
2017-04-27 01:59:21 -0400

    oooooh. shrunk state space to make it work better. also wrote functions to make changing limits easier

--- code/run.py
+++ code/run.py
@@ -6,8 +6,9 @@ from SwingyMonkey import SwingyMonkey
 
 from collections import defaultdict as dd
 
+START_EPSILON = 0.5
 EPSILON = 0.1
-LEARNING_RATE = 0.1
+LEARNING_RATE = 0.9
 DISCOUNT_FACTOR = 0.9
 # nested 6 layers for tree_dist, tree_top, tree_bot, monkey_vel, monkey_top, monkey_bot
 
@@ -17,24 +18,99 @@ class Learner(object):
     '''
 
     def __init__(self):
-        self.last_state  = None
-        self.last_action = None
-        self.last_reward = None
-        self.W = dd(lambda: dd(lambda: dd(lambda: dd(lambda: dd(lambda: dd(lambda: [0,0]))))))
+        self.reset()
+        print 'ehh'
+        self.count = 1
+        # self.W = dd(lambda: dd(lambda: dd(lambda: dd(lambda: dd(lambda: dd(lambda: [0,0]))))))
+        #grav[1,4]
+        #state space: velocity [+ or 0, 0 to -9 inclusive, lower]
+        # dist: 0-25, 25.1-50, 50.1-100, 100-200, 200+
+        # monkey_bot: 0-20, 20-60, 60-280, 280+
+        # monkey_bot_to_tree_bot: -100, -40, -15, 0, 0-15, 15-40, 40-100, 100+ 
+        #gravity (2) * velocity (3) * monkey_bot (4) * dist(5) *  monkey_bot_to_tree_bot (8) = ~? potential states
+
+        # self.W = dd(lambda: dd(lambda: dd(lambda: dd(lambda: dd(lambda: [0,0])))))
+        self.W = dd(lambda: dd(lambda: dd(lambda: [0,0]))) # minus gravity, velocity
+
 
     def reset(self):
         self.last_state  = None
         self.last_action = None
         self.last_reward = None
-        self.W = dd(lambda: dd(lambda: dd(lambda: dd(lambda: dd(lambda: dd(lambda: [0,0]))))))
+        self.gravity = None
+        self.epsil = START_EPSILON
 
-	def get_policy(self, state):
-		'''
-		returns 0 or 1
-		returns anticipated action based on state and self.W values by taking the maximum of the self.W values
-		'''
-		W_actions = self.W[t['dist']][t['top']][t['bot']][m['vel']][m['top']][m['bot']]
-		return W_actions.index(max(W_actions))
+    def get_W_parameters_array(self, state):
+        '''
+        Helper function for getting parameters needed to index into W table
+        returns an array of size Depth of W
+        '''
+        cur_t = state['tree']
+        cur_m = state['monkey']
+        if not self.gravity:
+            raise Exception("Need self.gravity defined before get_W_parameters_array() used")
+
+# { 'score': <current score>,
+#   'tree': { 'dist': <pixels to next tree trunk>,
+#          'top': <height of top of tree trunk gap>,
+#          'bot': <height of bottom of tree trunk gap> },
+#   'monkey': { 'vel': <current monkey y-axis speed>,
+#           'top': <height of top of monkey>,
+#           'bot': <height of bottom of monkey> }}
+        # grav = self.gravity == 4 #yes magic numbers
+
+        # vel = 0
+        # for limit in [-9, -999]:
+        #     if cur_m['vel'] > limit:
+        #         break
+        #     vel += 1
+
+        mbot = 0
+        for limit in [80, 270, 999]:
+            if cur_m['bot'] < limit:
+                break
+            mbot += 1
+
+        dist = 0
+        for limit in [75, 200, 999]:
+            if cur_t['dist'] < limit:
+                break
+            dist += 1
+            
+        mtbot = 0
+        for limit in [0, 40, 999]:
+            if cur_m['bot'] - cur_t['bot'] < limit:
+                break
+            mtbot += 1
+
+        return [mbot, dist, mtbot]
+
+
+    def get_W_action_array(self, state):
+        '''
+        returns array of size 2 representing W values for each action for a particular state
+        '''
+        cur_t = state['tree']
+        cur_m = state['monkey']
+
+        W_params = self.get_W_parameters_array(state)
+        cur_W = self.W
+        for p in W_params:
+            cur_W = cur_W[p]
+        #cur_W should be array now
+        assert len(cur_W) == 2
+        return cur_W
+
+    def get_policy(self, state):
+        '''
+        returns 0 or 1
+        returns anticipated action based on state and self.W values by taking the maximum of the self.W values
+        '''
+        cur_t = state['tree']
+        cur_m = state['monkey']
+
+        W_actions = self.get_W_action_array(state)
+        return W_actions.index(max(W_actions))
 
     def action_callback(self, state):
         '''
@@ -44,37 +120,57 @@ class Learner(object):
 
         # You might do some learning here based on the current state and the last state.
 
-# { ’score’: <current score>,
-#   ’tree’: { ’dist’: <pixels to next tree trunk>,
-#          ’top’: <height of top of tree trunk gap>,
-#          ’bot’: <height of bottom of tree trunk gap> },
-#   ’monkey’: { ’vel’: <current monkey y-axis speed>,
-#           ’top’: <height of top of monkey>,
-#           ’bot’: <height of bottom of monkey> }}
+# { 'score': <current score>,
+#   'tree': { 'dist': <pixels to next tree trunk>,
+#          'top': <height of top of tree trunk gap>,
+#          'bot': <height of bottom of tree trunk gap> },
+#   'monkey': { 'vel': <current monkey y-axis speed>,
+#           'top': <height of top of monkey>,
+#           'bot': <height of bottom of monkey> }}
 
         # You'll need to select and action and return it.
         # Return 0 to swing and 1 to jump
 
+        # calculate gravity if not yet done and can be done
+        if not self.gravity and self.last_state:
+            self.gravity = self.last_state['monkey']['vel'] - state['monkey']['vel']
 
-    	# SARSA off-policy model-free update
-        if state and self.last_state and self.last_action and self.last_reward:
-        	last_t = self.last_state['tree']
-        	last_m = self.last_state['monkey']
-        	cur_t = self.state['tree']
-        	cur_m = self.state['monkey']
+        if not self.gravity:
+            # first state; do nothing
+            self.last_action = 0
+            self.last_state  = state
+            return self.last_action
 
-        	# W_actions = self.W[last_t['dist']][t['top']][t['bot']][m['vel']][m['top']][m['bot']]
-        	self.W[last_t['dist']][last_t['top']][last_t['bot']][last_m['vel']][last_m['top']][last_m['bot']][self.last_action] -= LEARNING_RATE * (
-        		self.W[last_t['dist']][last_t['top']][last_t['bot']][last_m['vel']][last_m['top']][last_m['bot']][self.last_action] - (
-        			self.last_reward + DISCOUNT_FACTOR * self.W[cur_t['dist']][cur_t['top']][cur_t['bot']][cur_m['vel']][cur_m['top']][cur_m['bot']][self.get_policy(state)]
-        			)
-        		)
+        # SARSA off-policy model-free update
+        if state and self.last_state and self.last_action is not None and self.last_reward is not None:
+            last_t = self.last_state['tree']
+            last_m = self.last_state['monkey']
+            cur_t = state['tree']
+            cur_m = state['monkey']
+            
+            W_last_actions = self.get_W_action_array(self.last_state)
+            W_last_actions[self.last_action] -= LEARNING_RATE * (
+                W_last_actions[self.last_action] - (
+                    self.last_reward + DISCOUNT_FACTOR * self.get_W_action_array(state)[self.get_policy(state)]
+                    )
+                )
+            a = self.get_W_parameters_array(self.last_state)
+            print '%i :updated %i %i %i to %f' % (self.count, a[0], a[1], a[2], W_last_actions[self.last_action])
+            self.count += 1
+            if self.last_reward < 0:
+                print 'DEAD with %i' % self.last_reward 
+            # W_actions = self.W[last_t['dist']][t['top']][t['bot']][m['vel']][m['top']][m['bot']]
+            # self.W[last_t['dist']][last_t['top']][last_t['bot']][last_m['vel']][last_m['top']][last_m['bot']][self.last_action] -= LEARNING_RATE * (
+            #     self.W[last_t['dist']][last_t['top']][last_t['bot']][last_m['vel']][last_m['top']][last_m['bot']][self.last_action] - (
+            #         self.last_reward + DISCOUNT_FACTOR * self.W[cur_t['dist']][cur_t['top']][cur_t['bot']][cur_m['vel']][cur_m['top']][cur_m['bot']][self.get_policy(state)]
+            #         )
+            #     )
 
         # epsilon-greedy policy for minimizing loss, balancing exploration vs. exploitation
         if npr.rand() < EPSILON:
-	        new_action = npr.rand() < EPSILON
-	    else:
-	    	new_action = get_policy(state)
+            new_action = npr.rand() < 0.5
+        else:
+           new_action = self.get_policy(state)
 
         new_state  = state
 
@@ -117,16 +213,16 @@ def run_games(learner, hist, iters = 100, t_len = 100):
 
 if __name__ == '__main__':
 
-	# Select agent.
-	agent = Learner()
+    # Select agent.
+    agent = Learner()
 
-	# Empty list to save history.
-	hist = []
+    # Empty list to save history.
+    hist = []
 
-	# Run games. 
-	run_games(agent, hist, 20, 10)
+    # Run games. 
+    run_games(agent, hist, 20, 10)
 
-	# Save history. 
-	np.save('hist',np.array(hist))
+    # Save history. 
+    np.save('hist',np.array(hist))
 
 
